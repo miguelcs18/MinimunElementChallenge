868a4855deb70b5dfbb531b45ac82a95263c9f4b
miguelcs18 <miguelcs18@gmail.com>
2022-09-05 09:05:34 -0500

    Name of class fixed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        MinimumElement melement = new MinimumElement();
+        melement.readIntegers(5);
     }
 }
---
+++ src/MinimumElement.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.Scanner;
+public class MinimumElement {
+    public int[] readIntegers(int count)
+    {
+        Scanner scanner= new Scanner(System.in);
+        int[] array = new int[count];
+        for(int i=0; i<array.length; i++)
+        {
+            System.out.println("Enter integer number \r");
+            array[i]=scanner.nextInt();
+        }//for
+        System.out.println("array = " + Arrays.toString(array));
+        return array;
+    }
+}

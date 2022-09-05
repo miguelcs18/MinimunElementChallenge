4591c7bd1eb9edf77b339b8206ac44b05f14122f
miguelcs18 <miguelcs18@gmail.com>
2022-09-05 09:54:14 -0500

    Fixed logic for finMin() method.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
+import java.util.Arrays;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        System.out.println("Enter the number of integers to fin Minimum and press enter");
+        System.out.println("Enter the number of integers to fin Minimum and press enter \r");
         int count = scan.nextInt();
+        int[] array = new int[count];
+        int minimum;
         MinimumElement melement = new MinimumElement();
-        melement.readIntegers(count);
+        array = melement.readIntegers(count);
+        System.out.println("array after method findMin call = " + Arrays.toString(array));
+        minimum = melement.findMin(array);
+        System.out.println("Minimum element in the array is = " + minimum);
     }
 }
--- src/MinimumElement.java
+++ src/MinimumElement.java
@@ -10,25 +10,29 @@ public class MinimumElement {
             System.out.println("Enter integer number \r");
             array[i]=scanner.nextInt();
         }//for
-        System.out.println("array = " + Arrays.toString(array));
+        System.out.println("array readIntegers= " + Arrays.toString(array));
         return array;
     }
     public int findMin(int[] array)
     {
         boolean flag=true;
-        int temp = 0;
+        int temp;
         while(flag) {
             flag = false;
             for (int i = 0; i < array.length-1; i++) {
-                if(array[i] > array[i+1])
+                if (array[i] > array[i + 1])
                 {
-                    temp = array[i];
+                    System.out.println("array[i] = " +array[i] + " is greater than array[i+1]= " + array[i+1] );
+                    temp = array[i+1];
                     array[i+1] = array[i];
                     array[i] = temp;
+                    System.out.println("array[" + i + "] = " + array[i]);
+                    System.out.println("array[" + (i+1) + "] = " + array[i+1]);
                     flag = true;
                 }
             }//for
         } //while
+        System.out.println("array findMin = " + Arrays.toString(array));
         return array[0];
     }
 }
